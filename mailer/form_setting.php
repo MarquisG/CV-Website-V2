88514a1b7c3af5402254d7e3c33da7724c76129d
MarquisG <guillaume.marquis@edu.esiee.fr>
2017-10-01 14:27:14 +0100

    CV Website New version

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site CV";
+	$to = 'guillaume.marquis@edu.esiee.fr';
+	$from = 'guillaume.marquis@edu.esiee.fr';
+	$fromName = 'Guillaume Marquis';
+	$charset = 'UTF-8';
+?>
