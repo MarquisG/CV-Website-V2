a6512f950b8148cf870e0967c3b8b562415859cd
Guillaume Marquis <guillaume.marquis@lemonlearning.fr>
2018-09-15 10:53:39 +0200

    Bug fixing

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -1,8 +1,8 @@
 <?php
 	/*Form settings*/
 	$subj = "New message from the site CV";
-	$to = 'guillaume.marquis@edu.esiee.fr';
-	$from = 'guillaume.marquis@edu.esiee.fr';
+	$to = 'guillaume.marquis3@gmail.com';
+	$from = 'guillaume.marquis3@gmail.com';
 	$fromName = 'Guillaume Marquis';
 	$charset = 'UTF-8';
 ?>
